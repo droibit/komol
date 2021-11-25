0cf8fa0e99bf00b2095643a3df3e3a871f7ac8ce
Shinya Kumagai <roomful.rooms@gmail.com>
2021-11-25 16:56:18 +0900

    Surpress unused warning

--- komol-core/src/androidMain/kotlin/com/github/droibit/komol/LogcatLogger.kt
+++ komol-core/src/androidMain/kotlin/com/github/droibit/komol/LogcatLogger.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.github.droibit.komol
 
 import android.util.Log
--- komol-core/src/iosMain/kotlin/com/github/droibit/komol/KomoliOS.kt
+++ komol-core/src/iosMain/kotlin/com/github/droibit/komol/KomoliOS.kt
@@ -1,30 +1,32 @@
+@file:Suppress("unused")
+
 package com.github.droibit.komol
 
 import kotlin.native.concurrent.freeze
 import platform.Foundation.NSError
 
 fun Komol.v(error: NSError, message: String? = null) {
-    Komol.v(error.toThrowable(), message)
+    v(error.toThrowable(), message)
 }
 
 fun Komol.d(error: NSError, message: String? = null) {
-    Komol.d(error.toThrowable(), message)
+    d(error.toThrowable(), message)
 }
 
 fun Komol.i(error: NSError, message: String? = null) {
-    Komol.i(error.toThrowable(), message)
+    i(error.toThrowable(), message)
 }
 
 fun Komol.w(error: NSError, message: String? = null) {
-    Komol.w(error.toThrowable(), message)
+    w(error.toThrowable(), message)
 }
 
 fun Komol.e(error: NSError, message: String? = null) {
-    Komol.e(error.toThrowable(), message)
+    e(error.toThrowable(), message)
 }
 
 fun Komol.wtf(error: NSError, message: String? = null) {
-    Komol.wtf(error.toThrowable(), message)
+    wtf(error.toThrowable(), message)
 }
 
 private fun NSError.toThrowable(): Throwable {
