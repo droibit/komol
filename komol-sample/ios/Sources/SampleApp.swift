be28c7c0d7231a9a47f42a33d4312264317a38df
Shinya Kumagai <roomful.rooms@gmail.com>
2021-11-25 16:55:55 +0900

    iOS: Support building on Xcode13

--- komol-sample/ios/Sources/SampleApp.swift
+++ komol-sample/ios/Sources/SampleApp.swift
@@ -2,9 +2,7 @@ import SwiftUI
 
 @main
 struct SampleApp: App {
-    // swiftlint:disable weak_delegate
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    // swiftlint:enable weak_delegate
 
     var body: some Scene {
         WindowGroup {
